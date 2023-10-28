1571fd85f4c25c54d9ef620bd156c92a78868e11
Bay <bayurkp194@gmail.com>
2023-10-28 16:51:45 +0800

    Melakukan kustomisasi tema

--- app/customTheme.ts
+++ app/customTheme.ts
@@ -1,9 +1,21 @@
 import { extendTheme } from "@chakra-ui/react";
 
+const activeLabelStyles = {
+  transform: "scale(0.85) translateY(-24px)",
+};
+
 const customTheme = extendTheme({
   config: {
     initialColorMode: "system",
     useSystemColorMode: true,
+  },
+  breakpoints: {
+    base: "0px",
+    sm: "320px",
+    md: "768px",
+    lg: "960px",
+    xl: "1200px",
+    "2xl": "1536px",
   },
   colors: {
     forti: {
@@ -22,6 +34,37 @@ const customTheme = extendTheme({
   fonts: {
     heading: `'Montserrat', sans-serif`,
     body: `'Nunito', sans-serif`,
+  },
+  components: {
+    Form: {
+      variants: {
+        floating: {
+          container: {
+            _focusWithin: {
+              label: {
+                ...activeLabelStyles,
+              },
+            },
+            "input:not(:placeholder-shown) + label, .chakra-select__wrapper + label, textarea:not(:placeholder-shown) ~ label":
+              {
+                ...activeLabelStyles,
+              },
+            label: {
+              top: 0,
+              left: 0,
+              zIndex: 2,
+              position: "absolute",
+              backgroundColor: "white",
+              pointerEvents: "none",
+              mx: 3,
+              px: 1,
+              my: 2,
+              transformOrigin: "left top",
+            },
+          },
+        },
+      },
+    },
   },
 });
 
