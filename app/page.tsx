dcb755a8c5451a36d3a84a7ddf202b681c1e024a
Bay <bayurkp194@gmail.com>
2023-10-28 16:58:02 +0800

    Membuat halaman signup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
+"use client";
+
+import { Container } from "@chakra-ui/react";
 import Navbar from "./components/navbar";
+import QuestionCard from "./components/questionCard";
+import ColorModeToggle from "./components/colorModeToggle";
 
 export default function Home() {
   return (
     <>
       <Navbar />
+      <ColorModeToggle />
+      <Container maxW={"container.lg"} mt={4} centerContent>
+        <QuestionCard />
+      </Container>
     </>
   );
 }
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -33,11 +33,10 @@ export default function Login() {
   return (
     <>
       <Flex
-        width={"100vw"}
+        width={"100%"}
         height={"100vh"}
         alignContent={"center"}
         justifyContent={"center"}
-        overflow={"hidden"}
         shadow={"sm"}>
         <Center>
           <Box
